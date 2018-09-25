63f710966762c984f3ebbf8267434f97203ce0c3
Lars Hunze <lars.hunze@apostle.nl>
2018-09-25 13:43:51 +0200

    Add logic to download the image/video from internet before sharing it to Instagram

--- android/src/main/java/com/travelshelf/RNInstagramShareModule.java
+++ android/src/main/java/com/travelshelf/RNInstagramShareModule.java
@@ -17,33 +17,111 @@ import android.net.Uri;
 import android.app.Activity;
 import android.content.pm.PackageManager;
 import android.content.ComponentName;
+import android.support.v4.content.FileProvider;
+import android.provider.MediaStore;
+
+import java.io.BufferedInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Environment;
 
 public class RNInstagramShareModule extends ReactContextBaseJavaModule {
 
-  private ReactApplicationContext reactContext;
-  public RNInstagramShareModule(ReactApplicationContext reactContext) {
-    super(reactContext);
-    this.reactContext=reactContext;
-  }
-  @Override
-  public String getName() {
-    return "InstagramShare";
-  }
-  @ReactMethod
-  private void share(String type, String mediaPath){
+    private ReactApplicationContext reactContext;
+    public RNInstagramShareModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+        this.reactContext=reactContext;
+    }
+    @Override
+    public String getName() {
+        return "InstagramShare";
+    }
 
-    // Create the new Intent using the 'Send' action.
+    public Uri getVideoFromUrl(String src) {
+        // Set basic values
+        int count;
+        String tempFile = Environment.getExternalStorageDirectory().toString() + "/temp.mp4";
+
+        try {
+            URL url = new URL(src);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setDoInput(true);
+            connection.connect();
+
+            int lenghtOfFile = connection.getContentLength();
+
+            InputStream input = new BufferedInputStream(url.openStream(), 8192);
+            OutputStream output = new FileOutputStream(tempFile);
+
+            byte data[] = new byte[1024];
+
+            long total = 0;
+            while ((count = input.read(data)) != -1) {
+                total += count;
+
+                // writing data to file
+                output.write(data, 0, count);
+            }
+
+            // flushing output
+            output.flush();
+
+            // closing streams
+            output.close();
+            input.close();
+
+            // get the file which has been saved
+            File videoFile = new File(tempFile);
+
+            return Uri.parse(videoFile.getPath());
+        } catch (IOException e) {
+            return null;
+        }
+    }
+
+    public Uri getUriFromBitmap(String src) {
+        try {
+            URL url = new URL(src);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setDoInput(true);
+            connection.connect();
+            int lenghtOfFile = connection.getContentLength();
+            InputStream input = connection.getInputStream();
+            Bitmap imageBitmap = BitmapFactory.decodeStream(input);
+            ByteArrayOutputStream bytes = new ByteArrayOutputStream(lenghtOfFile);
+            imageBitmap.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+            String path = MediaStore.Images.Media.insertImage(this.reactContext.getContentResolver(), imageBitmap, "temp.jpg", "this is the description");
+            return Uri.parse(path);
+        } catch (IOException e) {
+            return null;
+        }
+    }
+
+    @ReactMethod
+    private void share(String type, String mediaPath)
+    {
         Intent share = new Intent(Intent.ACTION_SEND);
-        // Set the MIME type
         share.setPackage("com.instagram.android");
-        share.setType(type);
-        // Create the URI from the media
-        File media = new File(mediaPath);
-        Uri uri = Uri.fromFile(media);
 
-        // Add the URI to the Intent.
-        share.putExtra(Intent.EXTRA_STREAM, uri);
-        // Broadcast the Intent.
+        if(type.equals("image")) {
+            share.setType("image/*");
+            share.putExtra(Intent.EXTRA_STREAM, getUriFromBitmap(mediaPath));
+        }
+
+        if(type.equals("video")) {
+            share.setType("video/*");
+            share.putExtra(Intent.EXTRA_STREAM, getVideoFromUrl(mediaPath));
+        }
+
         share.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         reactContext.startActivity(share);
     }
